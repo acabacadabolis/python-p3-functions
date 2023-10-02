55c3d9ed1a3690b9d8b3e55dd8ebfcd6365cccb8
Kerem Deen <kerem.deen@gmail.com>
2023-10-02 11:22:27 -0400

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if(type(number) == type(12.354)): return number/2
+    if(type(number) == type(12)): return number/2
